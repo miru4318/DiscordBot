b7856431e73ef8c18448c4addb6833503d89e041
miru4318 <48778074+miru4318@users.noreply.github.com>
2019-11-08 15:47:22 +0900

    Add files via upload

---
+++ DiscrodBot.py
@@ -0,0 +1,36 @@
+import asyncio
+import discord
+import datetime
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('---------')
+    await client.change_presence(activity=discord.Game(name="Minecraft", type=1))
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!채널메시지"):
+        channel = message.content[7:25]
+        msg = message.content[26:]
+        await client.get_channel(int(channel)).send(msg)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!정보"):
+        date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
+        embed = discord.Embed(color=0x00ff00)
+        embed.add_field(name="이름", value=message.author.name, inline=True)
+        embed.add_field(name="서버이름", value=message.author.display_name, inline=True)
+        embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일", inline=True)
+        embed.add_field(name="아이디", value=message.author.id, inline=True)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        await message.channel.send(embed=embed)
+
+
+client.run("NjQwOTM0OTI5MTY0Nzk1OTA0.XcUCcg.ADBeu33r0G0ZPvjVJXDh44ol0wE")
